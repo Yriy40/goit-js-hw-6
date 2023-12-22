bad0d7f5ace48083d88a775224394443a9f6b8ef
Yriy40 <sirovyrij1986@gmail.com>
2023-12-22 14:24:56 +0200

    commit 2

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ const customer = {
   balance: 24000,
   discount: 0.1,
   orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
+
   getBalance() {
     return this.balance;
   },
@@ -20,11 +20,10 @@ const customer = {
     this.balance -= cost - cost * this.discount;
     this.orders.push(order);
   },
-  // Change code above this line
 };
 
-// customer.setDiscount(0.15);
-// console.log(customer.getDiscount()); // 0.15
-// customer.addOrder(5000, "Steak");
-// console.log(customer.getBalance()); // 19750
-// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
